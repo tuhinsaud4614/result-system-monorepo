c8018a40fc4f39d00feac32ff180278951d18c24
Md. Tuhin Saud <tuhinsaud4614@gmail.com>
2023-05-08 02:23:11 +0600

    issue fixed

--- libs/shared/utility/src/lib/main.ts
+++ libs/shared/utility/src/lib/main.ts
@@ -2,4 +2,4 @@
  * The function calculates the maximum file size in bytes based on the input in megabytes.
  * @param {number} mb - The parameter "mb" is a number representing the size of a file in megabytes.
  */
-export const maxFileSize = (mb: number) => mb * 1000000;
+export const maxFileSize = (mb: number) => mb || 1 * 1000000;
